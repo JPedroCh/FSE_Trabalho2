da012281171d9278cff79ad3a8fe6d3b784b4a1f
João Pedro Chaves <joaopedroaschaves@gmail.com>
2023-01-09 17:32:25 -0300

    Add error treatment to reading file from curva

--- curva.py
+++ curva.py
@@ -6,9 +6,12 @@ class Curva:
         self.temperatura = []
     
     def leitura_arquivo(self):
-        with open('curva_reflow.csv', 'r') as file:
-            reader = csv.DictReader(file)
-            
-            for row in reader:
-              self.tempo.append(int(row['Tempo (s)']))
-              self.temperatura.append(int(row[' Temperatura']))
+        try:
+          with open('curva_reflow.csv', 'r') as file:
+              reader = csv.DictReader(file)
+      
+              for row in reader:
+                self.tempo.append(int(row['Tempo (s)']))
+                self.temperatura.append(int(row[' Temperatura']))
+        except:
+            print("[ERRO] Arquivo configuração da curva pré-definida (curva_reflow.csv) não encontrado.")
